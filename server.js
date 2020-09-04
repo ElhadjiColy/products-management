02ad237eee152789fed94a7a3c51b2cdb7cadbb1
Elhadji <elhadji@ctsfares.com>
2020-09-04 05:48:29 -0400

    update dependencies

--- server.js
+++ server.js
@@ -2,24 +2,20 @@ var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 
-// @ts-ignore
 const history = require('connect-history-api-fallback')
 const app = express()
-// @ts-ignore
 const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))
 
 /* var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist'))) */
 
 app.use(staticFileMiddleware)
-// @ts-ignore
 app.use(history({
     disableDotRule: true,
     verbose: true
 }))
 app.use(staticFileMiddleware)
 app.get('/', function (req, res) {
-    // @ts-ignore
     res.render(path.join(__dirname + '/dist/index.html'))
 })
 
